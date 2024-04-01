ee05333367437b4f6ba2eb1da35da6f0932a65a0
LocTM <tranminhloc.anhk@gmail.com>
2024-04-01 08:39:44 +0700

    demo3

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Demo from "./Demo";
 import Demo2 from "./Demo2";
+import Demo3 from "./Demo3";
 
 function App() {
   return (
     <>
-      <Demo2></Demo2>
         <Demo></Demo>
+      <Demo3></Demo3>
+        <Demo2></Demo2>
+
+
     </>
   );
 }
---
+++ demo1/src/Demo3.js
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export default class Demo3 extends Component{
+    render(){
+        return(
+            <h1>ABC</h1>
+        )
+    }
+}
