a4ee384f1b28388c8b7cfa0c6a9d22bd71260a7c
LocTM <tranminhloc.anhk@gmail.com>
2024-04-01 09:06:54 +0700

    test3

--- demo1/src/Demo3.js
+++ demo1/src/Demo3.js
@@ -1,9 +1,15 @@
 import {Component} from "react";
 
 export default class Demo3 extends Component{
+    constructor() {
+        super();
+        this.state = {
+            name: "Test3"
+        }
+    }
     render(){
         return(
-            <h1>ABC</h1>
+            <h1>{this.state.name}</h1>
         )
     }
 }
